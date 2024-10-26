8b409f5e9915a3fff3b858bae77a767b37db6f2d
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-10-26 15:14:23 +0800

    1

--- monika.js
+++
@@ -1,9 +0,0 @@
-function addShadowPopBr(event) {
-    event.target.classList.add("monika-anim-shadow-pop-br");
-    event.target.classList.remove("monika-anim-shadow-pop-br-reverse");
-};
-
-function removeShadowPopBr(event) {
-    event.target.classList.add("monika-anim-shadow-pop-br-reverse");
-    event.target.classList.remove("monika-anim-shadow-pop-br");
-}
