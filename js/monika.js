e4a6710be2fbe4819dc7b0650b66a5c52167889f
Maiden1969 <zkr230527@mail.ustc.edu.cn>
2024-11-02 11:09:45 +0800

    1

--- js/monika.js
+++ js/monika.js
@@ -1,4 +1,4 @@
-const app = {
+const monika = {
     //从指定路径的JSON文件渲染页面数据
     render: (dataPath) => {
         // 读取 data.json 文件
@@ -6,27 +6,31 @@ const app = {
             .then(response => response.json())
             .then(data => {
                 const my_app = document.body;
-                app.replacePlaceholders(my_app, data);
+                monika.replacePlaceholders(my_app, data);
             })
             .catch(error => console.error('Error fetching data:', error));
 
     },
-
 
     // 替换占位符，渲染列表
     // 递归访问每个节点以及其子节点
     replacePlaceholders: (node, data) => {
         if (node.nodeType === Node.ELEMENT_NODE) {
             if (node.id[0] === '#') {
-                // const container = document.getElementById(node.id);  //获得项目容器
-                // const item = container.firstElementChild;  //获得项目模版
-                // container.removeChild(item); //删除模版
-                // let pos = 1;
-                // while (pos <= 4) {
-                //     const clonedItem = item.cloneNode(true);
-                //     app.replacePlaceholdersForItem(clonedItem, data, `${pos}`);
-                //     pos++;
-                //     container.appendChild(clonedItem);
+                const container = document.getElementById(node.id);  //获得项目容器
+                const item = container.firstElementChild;  //获得项目模版
+                container.removeChild(item); //删除模版
+                let pos = 1;
+                while (1) {
+                    const clonedItem = item.cloneNode(true);
+                    const flag = monika.replacePlaceholdersForItem(clonedItem, data, pos);
+                    if (flag) {
+                        container.appendChild(clonedItem);
+                        pos++;
+                    }
+                    else
+                        break;
+                }
             }
             // 遍历所有属性
             for (let i = 0; i < node.attributes.length; i++) {
@@ -63,15 +67,12 @@ const app = {
         }
         // 递归遍历子节点
         for (let child of node.childNodes) {
-            app.replacePlaceholders(child, data);
+            monika.replacePlaceholders(child, data);
         }
 
         function getNestedValue(obj, keyPath) {
             return keyPath.split('.').reduce((acc, key) => acc && acc[key], obj);
         }
-
-
-
     },
 
     //为列表的每个项目节点替换占位符
@@ -85,7 +86,8 @@ const app = {
                 let res_atr = '';
                 let lastIndex_atr = 0;
                 while ((match_atr = regex.exec(attr.value)) !== null) {
-                    const value = match_atr[0] + pos;
+                    const value = getNestedValue(data,match_atr[1] + pos);
+                    if (typeof value === 'undefined') return 0;
                     res_atr += attr.value.slice(lastIndex_atr, match_atr.index);
                     res_atr += value;
                     lastIndex_atr = match_atr.index + match_atr[0].length;
@@ -103,7 +105,8 @@ const app = {
             let res_text = '';
             let lastIndex_text = 0;
             while ((match_text = regex.exec(node.textContent)) !== null) {
-                const value = match_text[0] + pos;
+                const value = getNestedValue(data, match_text[1] + pos);
+                if (typeof value === 'undefined') return 0;
                 res_text += node.textContent.slice(lastIndex_text, match_text.index);
                 res_text += value;
                 lastIndex_text = match_text.index + match_text[0].length;
@@ -113,12 +116,28 @@ const app = {
         }
         // 递归遍历子节点
         for (let child of node.childNodes) {
-            app.replacePlaceholdersForItem(child, data, pos);
+            const flag = monika.replacePlaceholdersForItem(child, data, pos);
+            if (flag == 0)
+                return 0;
         }
 
+        function getNestedValue(obj, keyPath) {
+            return keyPath.split('.').reduce((acc, key) => acc && acc[key], obj);
+        }
+
+        return 1;
     },
 
+    //简化的ID选择器
+    $ :(id) => {
+        return document.getElementById(id);
+    },
+
+    //简化的标签、类选择器
+    $$ :(selector) => {
+        return document.querySelectorAll(selector);
+    },
 };
 
+export default monika;
 
-export default app;
